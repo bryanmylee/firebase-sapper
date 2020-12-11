a727bd08276fa834058b8ed8c1ab7818800a185a
Bryan Lee <bryanmylee@gmail.com>
2020-12-11 17:46:58 +0800

    implement logout and check for user id

--- src/routes/dashboard.svelte
+++ src/routes/dashboard.svelte
@@ -1,12 +1,25 @@
 <!-- To prevent flash of content, we check if user exists before rendering the page.
      Therefore, we perform the auth check in preload function. -->
 <script context="module" lang="ts">
+  import { goto } from "@sapper/app";
   import type Common from "@sapper/common";
+  import firebase from "firebase/app";
+
+  let user: string | boolean = false;
 
   export const preload: Common.Preload = async function(this, page, session) {
-    const { user } = session;
+    user = session.user;
     if (!user) {
-      return this.redirect(302, '/login');
+      return this.redirect(302, "/login");
     }
   };
+
+  const logout = async () => {
+    await firebase.auth().signOut();
+    goto("/login");
+  }
 </script>
+
+<h1>This dashboard is protected!</h1>
+<p>Current user: {user}</p>
+<button on:click={logout}>Logout</button>
